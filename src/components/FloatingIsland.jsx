883d6dd78e2531da60f945cc040929ecd79fc3ef
Vivek <vivek@vivek>
2026-04-18 02:14:39 +0530

    new change

--- src/components/FloatingIsland.jsx
+++ src/components/FloatingIsland.jsx
@@ -1234,7 +1234,7 @@ function FloatingIslandScene({ onSelect }) {
       <Tree position={[2.0, 0.28, -1.3]} />
       {/* <Tree position={[-1.5, 0.28, -1.9]} /> */}
       <Lantern position={[-1.7, 0.28, -0.5]} />
-      <Campfire position={[1.50, 0.28, 1.80]} />
+      <Campfire position={[1.75, 0.28, 1.75]} />
       <Mailbox onSelect={onSelect} />
       <WelcomeSign onSelect={onSelect} />
       <ProjectCanvas onSelect={onSelect} />
